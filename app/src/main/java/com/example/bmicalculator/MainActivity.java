fe17afef71320b20b3b5cd1bf99390d27ebe0dde
Jitesh Bhashwani <jitubhashwani999@gmail.com>
2023-03-20 21:57:04 +0530

    Init commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txtResult = findViewById(R.id.result);
+        EditText edtWeight = findViewById(R.id.edt_weight);
+        EditText edtFtHeight = findViewById(R.id.edt_height_ft);
+        EditText edtInHeight = findViewById(R.id.edt_height_inc);
+        Button btnSubmit = findViewById(R.id.btn_calculate_submit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int weight = Integer.parseInt(edtWeight.getText().toString());
+                    int ftHeight = Integer.parseInt(edtFtHeight.getText().toString());
+                    int inHeight = Integer.parseInt(edtInHeight.getText().toString());
+                    float heightInMtr = ((ftHeight*12 + inHeight) * 2.54f) / 100;
+                    if(heightInMtr == 0) {
+                        txtResult.setText("invalid input: height cannot be zero");
+                    } else {
+                        double bmi = weight / (heightInMtr * heightInMtr);
+                        Log.d("BMI Calculation", "Weight: " + weight + " kg, Height: " + ftHeight + "'" + inHeight + "\", BMI: " + bmi);
+                        if (bmi < 18.5) {
+                            txtResult.setText("Underweight");
+                        } else if (bmi < 25) {
+                            txtResult.setText("Normal weight");
+                        } else if (bmi < 30) {
+                            txtResult.setText("Overweight");
+                        } else {
+                            txtResult.setText("Obese");
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    txtResult.setText("invalid input: please enter valid numbers");
+                }
+            }
+        });
+    }
+}
